61f68d4fdd408c34e1459ec1efc61b13ea162ae5
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-10-10 16:42:41 +0300

    refactor(demo): adds a few common constants and improves descriptions

--- demo.ts
+++ demo.ts
@@ -11,6 +11,10 @@ import { privateKeyToAccount } from "viem/accounts";
 import { celoAlfajores } from "viem/chains";
 import "dotenv/config"; // use to read private key from environment variable
 
+// Constants
+const EXPLORER_URL = 'https://alfajores.celoscan.io/tx/';
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 /**
  * Boilerplate to create a client
  */
@@ -18,17 +22,41 @@ const publicClient = createPublicClient({
     chain: celoAlfajores,
     transport: http(),
 });
-const account = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
+const account = privateKeyToAccount(`0x${PRIVATE_KEY}`);
 const client = createWalletClient({
     chain: celoAlfajores, // Celo testnet
     transport: http(),
 });
 
 /**
+ * Legacy transaction type
+ */
+async function demoLegacyTransactionType() {
+    console.log(`Initiating legacy transaction...`);
+    const transactionHash = await client.sendTransaction({
+        account, // Sender
+        to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", // Recipient (illustrative address)
+        value: parseEther("0.01"), // 0.01 CELO
+        gasPrice: parseGwei("20"), // Special field for legacy transaction type
+    });
+    console.log(`Legacy transaction:`, transactionHash, "\n");
+
+    const transactionReceipt = await publicClient.waitForTransactionReceipt({
+        hash: await transactionHash,
+    });
+    console.log(`Legacy transaction receipt`, transactionReceipt, "\n");
+    console.log(
+        `See in explorer:`,
+        `${EXPLORER_URL}${transactionReceipt.transactionHash}`,
+        "\n"
+    );
+}
+
+/**
  * Dynamic fee transaction type (EIP-1559)
  */
 async function demoDynamicFeeTransactionType() {
-    console.log(`Starting dynamic fee (EIP-1559) transaction type demo`);
+    console.log(`Initiating dynamic fee (EIP-1559) transaction...`);
     const transactionHash = await client.sendTransaction({
         account, // Sender
         to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", // Recipient (illustrative address)
@@ -36,43 +64,28 @@ async function demoDynamicFeeTransactionType() {
         maxFeePerGas: parseGwei("10"), // Special field for dynamic fee transaction type (EIP-1559)
         maxPriorityFeePerGas: parseGwei("10"), // Special field for dynamic fee transaction type (EIP-1559)
     });
-    console.log(`Dynamic fee (EIP-1559) transaction`, transactionHash);
+    console.log(`Dynamic fee (EIP-1559) transaction:`, transactionHash, "\n");
 
     const transactionReceipt = await publicClient.waitForTransactionReceipt({
         hash: await transactionHash,
     });
     console.log(
         `Dynamic fee (EIP-1559) transaction receipt`,
-        transactionReceipt
+        transactionReceipt,
+        "\n"
     );
-}
-
-/**
- * Legacy transaction type
- */
-async function demoLegacyTransactionType() {
-    console.log(`Starting legacy transaction type demo`);
-    const transactionHash = await client.sendTransaction({
-        account, // Sender
-        to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", // Recipient (illustrative address)
-        value: parseEther("0.01"), // 0.01 CELO
-        gasPrice: parseGwei("20"), // Special field for legacy transaction type
-    });
-    console.log(`Legacy transaction`, transactionHash);
-
-    const transactionReceipt = await publicClient.waitForTransactionReceipt({
-        hash: await transactionHash,
-    });
-    console.log(`Legacy transaction receipt`, transactionReceipt);
+    console.log(
+        `See in explorer:`,
+        `${EXPLORER_URL}${transactionReceipt.transactionHash}`,
+        "\n"
+    );
 }
 
 // Wrap both demos in an async function to await their completion
 async function runDemosSequentially() {
-    // Run the first demo and await its completion
-    await demoDynamicFeeTransactionType();
-
-    // Run the second demo and await its completion
+    // Run each demo and await its completion
     await demoLegacyTransactionType();
+    await demoDynamicFeeTransactionType();
 }
 
 // Run the demos sequentially
